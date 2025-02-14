83a9395c65b71b239aa1357874373f821999f6f3
JulyannaGama <julyannagama6@gmaill.com>
2025-02-14 17:02:02 -0300

    ajustes

--- ramon-freire/src/components/Hero/Hero.jsx
+++ ramon-freire/src/components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Hero.scss";
 import imgBanner from "../../assets/imgBanner.png";
-import Typewriter from "typewriter-effect";
+import { Typewriter } from "react-simple-typewriter";
 import { FaDumbbell, FaWeightHanging, FaRunning, FaHeartbeat } from "react-icons/fa";
 
 const whatsappNumber = "5575998587858";
@@ -14,7 +14,6 @@ const handleWhatsAppClick = (message) => {
 const Hero = () => {
   return (
     <section className="hero">
-      {/* Itens flutuantes */}
       <FaDumbbell className="floating-icon icon-1" />
       <FaWeightHanging className="floating-icon icon-2" />
       <FaRunning className="floating-icon icon-3" />
@@ -23,13 +22,13 @@ const Hero = () => {
       <div className="hero-content">
         <h1 className="hero-title">
           <Typewriter
-            options={{
-              strings: ["TRANSFORME SEU CORPO", "COM DISCIPLINA E FOCO"],
-              autoStart: true,
-              loop: true,
-              deleteSpeed: 50,
-              delay: 80,
-            }}
+            words={["TRANSFORME SEU CORPO", "COM DISCIPLINA E FOCO"]}
+            loop={true}
+            cursor
+            cursorStyle="_"
+            typeSpeed={80}
+            deleteSpeed={50}
+            delaySpeed={1000}
           />
         </h1>
         <p>
@@ -38,7 +37,12 @@ const Hero = () => {
           também a mente. Junte-se à maior academia de Esplanada e conquiste
           seus objetivos com acompanhamento profissional e resultados reais.
         </p>
-        <button className="cta-button" onClick={() => handleWhatsAppClick("Olá! Quero mudar de vida e transformar meu corpo. Podemos conversar?")}>
+        <button
+          className="cta-button"
+          onClick={() =>
+            handleWhatsAppClick("Olá! Quero mudar de vida e transformar meu corpo. Podemos conversar?")
+          }
+        >
           COMEÇAR AGORA
         </button>
       </div>
